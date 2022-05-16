76940151c1fc7993fa13e9ac2488bb0c9bbf43e9
Shravan <shravankashyap95@gmail.com>
2022-05-16 13:56:04 +0530

    Update port

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-const port = process.env.PORT || 3000;
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 3001;
+const cors = require("cors");
+const path = require("path");
 const app = express();
-const polls = require('./routes/poll');
+const polls = require("./routes/poll");
 
 mongoose
-  .connect('mongodb://localhost/live-poll-application', {
+  .connect("mongodb://localhost/live-poll-application", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
-  .then(() => console.log('connected to mongoDb'))
-  .catch(err => console.log('Error occured: ', err));
+  .then(() => console.log("connected to mongoDb"))
+  .catch((err) => console.log("Error occured: ", err));
 
 app.use(cors());
-app.set('view engine', 'ejs');
-app.use('/static/', express.static(path.join(__dirname + '/static')));
-app.use('/', polls);
+app.set("view engine", "ejs");
+app.use("/static/", express.static(path.join(__dirname + "/static")));
+app.use("/", polls);
 app.use(express.json());
 
-app.get('*', (req, res) => {
-  res.status(404).render('notfound');
+app.get("*", (req, res) => {
+  res.status(404).render("notfound");
 });
 
 app.listen(port, () => {
